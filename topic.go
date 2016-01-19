be3a559cdec4485b769def6e54417b9b04e52c25
Jorge Olivero <jorgeaolivero@gmail.com>
2016-01-18 22:56:12 -0500

    fixes to topic types

--- topic.go
+++ topic.go
@@ -23,23 +23,23 @@ func (res TopicResponse) String() string {
 }
 
 // Req returns the request version of a topic.
-func (t Topic) Req() Topic {
+func (t Topic) Req() TopicRequest {
 	return TopicRequest(fmt.Sprintf("%s.%s", t.String(), "REQ"))
 }
 
 // Res returns the response version of a topic.
-func (t Topic) Res() Topic {
+func (t Topic) Res() TopicResponse {
 	return TopicResponse(fmt.Sprintf("%s.%s", t.String(), "RES"))
 }
 
 // ResUnique returns a unique subject where a response to
 // a Topic request will be routed.
-func (t Topic) ResUnique() string {
+func (t Topic) ResUnique() TopicResponse {
 	return TopicResponse(fmt.Sprintf("%s.%s.%s", t.String(), "RES", uuid.New()))
 }
 
 // ResWildward returns a wild card subject where all
 // responses to a topic request will be routed.
-func (t Topic) ResWildcard() string {
+func (t Topic) ResWildcard() TopicResponse {
 	return TopicResponse(fmt.Sprintf("%s.%s.%s", t.String(), "RES", "*"))
 }
